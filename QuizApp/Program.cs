5bfc5a109d63a40c6cc1540dc11621947e6932c1
Adrian <Zilibdev@gmail.com>
2020-04-09 15:01:00 +0200

    First commit, create quiz class. Client can make choose

---
+++ QuizApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace QuizApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greetings();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Tell greetings to user!
+        /// </summary>
+        static void Greetings()
+        {
+            Console.WriteLine("***** Quiz Application ******\n");
+            Console.WriteLine("What would you like to do?");
+            Console.Write("Take [1] for create new quiz, [2] for take part of another one: ");
+
+            string input = Console.ReadLine();
+            // if input is not 1 or 2, ask for another input
+            while (input != "1" && input != "2")
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input, please make your choose again!");
+                Console.Write("Take [1] for create new quiz, [2] for take part of another one: ");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ QuizApp/Quiz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizApp
+{
+    class Game
+    {
+        #region Private variables
+
+        // Number of all quizes
+        private List<Quiz> Quizes = new List<Quiz>();
+
+        #endregion
+
+        #region Public methods 
+
+        public int GetNumberOfQuizes => Quizes.Count;
+
+        #endregion
+    }
+
+    class Quiz
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}

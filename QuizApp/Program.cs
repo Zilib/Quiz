200ce3ecd0ac1c2791dc91bf3ea89a676950b0a2
Adrian <Zilibdev@gmail.com>
2020-04-09 16:00:51 +0200

    Simple models, and implementing methods. Create question, quiz.

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -6,20 +6,29 @@ namespace QuizApp
     {
         static void Main(string[] args)
         {
-            Greetings();
+            Game myQuizGame = new Game();
+            Greetings(out string input);
+
+            switch (input)
+            {
+                case "1":
+                    myQuizGame.CreateNewQuiz();
+                    break;
+            }
+
             Console.ReadLine();
         }
 
         /// <summary>
-        /// Tell greetings to user!
+        /// Tell greetings to user! Let him make choose what he want to do
         /// </summary>
-        static void Greetings()
+        static void Greetings(out string input)
         {
             Console.WriteLine("***** Quiz Application ******\n");
             Console.WriteLine("What would you like to do?");
             Console.Write("Take [1] for create new quiz, [2] for take part of another one: ");
 
-            string input = Console.ReadLine();
+            input = Console.ReadLine();
             // if input is not 1 or 2, ask for another input
             while (input != "1" && input != "2")
             {
@@ -29,5 +38,7 @@ namespace QuizApp
                 input = Console.ReadLine();
             }
         }
+
+        
     }
 }
--- QuizApp/Quiz.cs
+++ QuizApp/Quiz.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace QuizApp
@@ -12,17 +13,133 @@ namespace QuizApp
 
         #endregion
 
-        #region Public methods 
+        #region Public lambda methods 
 
         public int GetNumberOfQuizes => Quizes.Count;
 
         #endregion
+
+        #region Public methods
+
+        public void CreateNewQuiz()
+        {
+            Console.Clear();
+            Quiz tempQuiz = new Quiz();
+
+            #region Set quiz main data
+
+            Console.WriteLine("Please input quiz title");
+            tempQuiz.Title = Console.ReadLine();
+            Validators.ValidString(tempQuiz.Title, 4, "Please input quiz title");
+
+            Console.Clear();
+            Console.WriteLine("Please input quiz description");
+            tempQuiz.Description = Console.ReadLine();
+            Validators.ValidString(tempQuiz.Description, 20, "Please input quiz description");
+
+            #endregion 
+
+            Console.Clear();
+            Console.WriteLine("How many questions do you want? ( More than 2 and less than 10)");
+
+            string input = Console.ReadLine();
+            while(!Int32.TryParse(input, out int numberOfQuestions) 
+                || (numberOfQuestions <= 2 
+                || numberOfQuestions >= 10))
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input!");
+                Console.WriteLine("How many questions do you want? ( More than 2 and less than 10)");
+                input = Console.ReadLine();
+            }
+
+
+        }
+
+        #endregion
+
     }
 
-    class Quiz
+    sealed class Quiz
     {
+        private List<Question> Questions { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
 
+        public void CreateQuestion()
+        {
+            Console.Clear();
+            Console.WriteLine("Give your question text:");
+
+            string tempText = Console.ReadLine();
+            Validators.ValidString(tempText, 10, "Give your text:");
+
+            Questions.Add(new Question(tempText));
+        }
+    }
+
+    /// <summary>
+    /// Every important things for quiz texts
+    /// </summary>
+    abstract class FieldVariables
+    {
+        public FieldVariables(string title)
+        {
+            Text = title;
+        }
+
+        public string Text { get; set; }
+    }
+
+    sealed class Question : FieldVariables
+    {
+        private List<Answer> answers;
+
+        public Question(string title)
+            : base(title)
+        {
+            CreateAnswers();
+        }
+
+        /// <summary>
+        /// Create 4 answers
+        /// </summary>
+        private void CreateAnswers()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Clear();
+            }
+        }
+    }
+
+    sealed class Answer : FieldVariables
+    {
+        public Answer(string title)
+            : base (title)
+        {
+
+        }
+        public bool IsCorrect { get; set; }
+    }
+
+    static class Validators
+    {
+        /// <summary>
+        /// Wait for user input untill he's input does not meet the requirements
+        /// </summary>
+        /// <param name="input">Property</param>
+        /// <param name="minLength"></param>
+        /// <param name="msg">Request for new input</param>
+        public static void ValidString(string input, int minLength, string msg)
+        {
+            while (input.Length <= 3)
+            {
+                Console.Clear();
+                Console.WriteLine($"Input is too short, minimum length is {minLength}");
+                Console.WriteLine(msg);
+                input = Console.ReadLine();
+            }
+        }
     }
 }
